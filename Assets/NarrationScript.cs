48f3899226171a29dbf020d150d582db0f07d54d
Eddie <eddielusanchez@gmail.com>
2020-02-01 13:06:03 -0800

    Basic canvas with dialogue options to use during transitions for the different scenes

---
+++ Assets/NarrationScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NarrationScript : MonoBehaviour
+{
+    public GameObject TextBox;
+    public GameObject Option01;
+    public GameObject Option02;
+    public GameObject Option03;
+    public int OptionSelected;
+
+    public void optionSelected1()
+    {
+        TextBox.GetComponent<Text>().text = "Yummy, bones!";
+        OptionSelected = 1;
+    }
+
+    public void optionSelected2()
+    {
+        TextBox.GetComponent<Text>().text = "Isn't that where we first met?!";
+        OptionSelected = 2;
+    }
+
+    public void optionSelected3()
+    {
+        TextBox.GetComponent<Text>().text = "I love walks!";
+        OptionSelected = 3;
+    }
+    
+    void Update()
+    {
+        if (OptionSelected >= 1)
+        {
+            Option01.SetActive(false);
+            Option02.SetActive(false);
+            Option03.SetActive(false);
+        }
+        
+    }
+}
