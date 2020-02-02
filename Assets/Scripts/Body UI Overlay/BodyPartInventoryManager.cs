68d6ed20dec37806f1b6da8244d86a5d9f1c0d4c
Matt <Osafune24@gmail.com>
2020-02-01 19:40:24 -0800

    Body Part Inventory Updates

--- Assets/Scripts/Body UI Overlay/BodyPartInventoryManager.cs
+++ Assets/Scripts/Body UI Overlay/BodyPartInventoryManager.cs
@@ -13,27 +13,101 @@ namespace DespairRepair
         public const string LEFT_LEG_TAG = "UI-Left-Leg";
         public const string RIGHT_LEG_TAG = "UI-Right-Leg";
 
+        private const int TOTAL_BODY_PART_COUNT = 6;
+
+        private ArrayList collectedParts;
+        private int correctPartCount;
+
+
+        private int test = 0;
+
         // Start is called before the first frame update
         void Start()
         {
-                
+            this.collectedParts = new ArrayList();
+            this.correctPartCount = 0;
         }
 
         // Update is called once per frame
         void Update()
         {
-            this.CollectBodyPart(BodyPartTypes.torso, true);
+            if (test == 0) 
+            { 
+                this.CollectBodyPart(BodyPartTypes.torso, true);
+                this.CollectBodyPart(BodyPartTypes.leftArm, false);
+                this.CollectBodyPart(BodyPartTypes.rightLeg, true);
+                this.CollectBodyPart(BodyPartTypes.head, false);
+                this.RemoveBodyPart(BodyPartTypes.head);
+                this.RemoveBodyPart(BodyPartTypes.torso);
+                this.RemoveLastBodyPart();
+                this.RemoveLastBodyPart();
+
+                print(this.AreAllBodyPartsCollected());
+                print(this.GetNumberOfCorrectBodyPartsCollected());
+                
+                test++;
+            }
         }
 
         public void CollectBodyPart(BodyPartTypes bodyPartType, bool isCorrectPart)
         {
             UIBodyPart part = this.GetBodyPart(bodyPartType);
-            print(part);
-
+            
             if (part != null && !part.IsCollected())
             {
                 part.Collect(isCorrectPart);
+                this.collectedParts.Add(part);
+
+                if (isCorrectPart)
+                {
+                    this.correctPartCount++;
+                }
             }
+        }
+
+        public UIBodyPart RemoveLastBodyPart()
+        {
+            if (this.collectedParts.Count > 0)
+            {
+                UIBodyPart lastBodyPart = (UIBodyPart)this.collectedParts[this.collectedParts.Count-1];   //this.collectedParts[this.collectedPartCount - 1];
+                if (lastBodyPart != null)
+                {
+                    if (lastBodyPart.IsCorrect())
+                    {
+                        this.correctPartCount--;
+                    }
+
+                    lastBodyPart.Uncollect();
+                    this.collectedParts.RemoveAt(this.collectedParts.Count - 1);
+
+                    return lastBodyPart;
+                }
+                
+                return null;
+            }
+
+            return null;
+        }
+
+        public UIBodyPart RemoveBodyPart(BodyPartTypes bodyPartType)
+        {
+            foreach (UIBodyPart bodyPart in this.collectedParts)
+            {
+                if (bodyPart.partType == bodyPartType)
+                {
+                    this.collectedParts.Remove(bodyPart);
+                    if (bodyPart.IsCorrect())
+                    {
+                        this.correctPartCount--;
+                        print(this.correctPartCount);
+                    }
+                    bodyPart.Uncollect();
+
+                    return bodyPart;
+                }
+            }
+
+            return null;
         }
 
         public bool IsBodyPartCollected(BodyPartTypes bodyPartType)
@@ -52,10 +126,9 @@ namespace DespairRepair
         {
             UIBodyPart part = null;
             UIBodyPart[] partList = GameObject.FindObjectsOfType<UIBodyPart>();
-            print(this.GetComponentsInChildren<UIBodyPart>().Length);
+              
             foreach (UIBodyPart bodyPart in partList)
             {
-                print(bodyPart.partType);
                 if (bodyPart.partType == partType)
                 {
                     part = bodyPart;
@@ -67,31 +140,12 @@ namespace DespairRepair
 
         public bool AreAllBodyPartsCollected()
         {
-            foreach(UIBodyPart bodyPart in this.GetComponentsInChildren<UIBodyPart>())
-            {
-                if (!bodyPart.IsCollected())
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return this.collectedParts.Count == TOTAL_BODY_PART_COUNT;
         }
 
         public int GetNumberOfCorrectBodyPartsCollected()
         {
-            int count = 0;
-
-            foreach (UIBodyPart bodyPart in this.GetComponentsInChildren<UIBodyPart>())
-            {
-                if (bodyPart.IsCorrect())
-                {
-                    count++;
-                }
-            }
-
-            return count;
+            return this.correctPartCount;
         }
     }
-    
 }
--- Assets/Scripts/Body UI Overlay/UIBodyPart.cs
+++ Assets/Scripts/Body UI Overlay/UIBodyPart.cs
@@ -15,13 +15,14 @@ namespace DespairRepair
 
         private const string SPRITE_DIRECTORY = "Sprites/body parts/";
 
-        private const string HEAD_COLLECTED_SPRITE = "head-collected";
-        private const string TORSO_COLLECTED_SPRITE = "torso-collected";
-        private const string LEFT_ARM_COLLECTED_SPRITE = "left-arm-collected";
-        private const string RIGHT_ARM_COLLECTED_SPRITE = "right-arm-collected";
-        private const string LEFT_LEG_COLLECTED_SPRITE = "left-leg-collected";
-        private const string RIGHT_LEG_COLLECTED_SPRITE = "right-leg-collected";
-
+        private const string HEAD_SPRITE = "head";
+        private const string TORSO_SPRITE = "torso";
+        private const string LEFT_ARM_SPRITE = "left-arm";
+        private const string RIGHT_ARM_SPRITE = "right-arm";
+        private const string LEFT_LEG_SPRITE = "left-leg";
+        private const string RIGHT_LEG_SPRITE = "right-leg";
+        
+        private const string COLLECTED = "-collected";
         private const string INCORRECT_SUFFIX = "-incorrect";
 
 
@@ -52,30 +53,63 @@ namespace DespairRepair
 
             if (this.partType == BodyPartTypes.head)
             {
-                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + HEAD_COLLECTED_SPRITE + incorrectSuffix);
+                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + HEAD_SPRITE + COLLECTED + incorrectSuffix);
             }
             else if (this.partType == BodyPartTypes.torso)
             {
-                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + TORSO_COLLECTED_SPRITE + incorrectSuffix);
+                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + TORSO_SPRITE + COLLECTED + incorrectSuffix);
             }
             else if (this.partType == BodyPartTypes.leftArm)
             {
-                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + LEFT_ARM_COLLECTED_SPRITE + incorrectSuffix);
+                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + LEFT_ARM_SPRITE + COLLECTED + incorrectSuffix);
             }
             else if (this.partType == BodyPartTypes.rightArm)
             {
-                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + RIGHT_ARM_COLLECTED_SPRITE + incorrectSuffix);
+                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + RIGHT_ARM_SPRITE + COLLECTED + incorrectSuffix);
             }
             else if (this.partType == BodyPartTypes.leftLeg)
             {
-                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + LEFT_LEG_COLLECTED_SPRITE + incorrectSuffix);
+                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + LEFT_LEG_SPRITE + COLLECTED + incorrectSuffix);
             }
             else if (this.partType == BodyPartTypes.rightLeg)
             {
-                this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + RIGHT_LEG_COLLECTED_SPRITE + incorrectSuffix);
+               this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + RIGHT_LEG_SPRITE + COLLECTED + incorrectSuffix);
             }
 
             //this.spriteRenderer.color = Color.red;//211,29,0
+        }
+
+        public void Uncollect()
+        {
+            if (this.isCollected)
+            {
+                this.isCollected = false;
+                
+                if (this.partType == BodyPartTypes.head)
+                {
+                    this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + HEAD_SPRITE);
+                }
+                else if (this.partType == BodyPartTypes.torso)
+                {
+                    this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + TORSO_SPRITE);
+                }
+                else if (this.partType == BodyPartTypes.leftArm)
+                {
+                    this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + LEFT_ARM_SPRITE);
+                }
+                else if (this.partType == BodyPartTypes.rightArm)
+                {
+                    this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + RIGHT_ARM_SPRITE);
+                }
+                else if (this.partType == BodyPartTypes.leftLeg)
+                {
+                    this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + LEFT_LEG_SPRITE);
+                }
+                else if (this.partType == BodyPartTypes.rightLeg)
+                {
+                    this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + RIGHT_LEG_SPRITE);
+                }
+            }
         }
 
         public bool IsCollected()
