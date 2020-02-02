50a00ace1c03c6d8699dfea3e078a15e123c0c02
Eddie <eddielusanchez@gmail.com>
2020-02-01 20:48:04 -0800

    Added screenDelay to manage transition speed and updated main menu button colors

--- Assets/NarrationScript.cs
+++ Assets/NarrationScript.cs
@@ -13,46 +13,50 @@ public class NarrationScript : MonoBehaviour
     public GameObject Option05;
     public GameObject Option06;
     public int OptionSelected;
-  
+    public string levelToLoad;
+    [SerializeField]
+    float screenDelay = 1.5f;
+
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+        SceneManager.LoadScene(levelToLoad, LoadSceneMode.Single);
+    }
+
 
     public void optionSelected1()
     {
         TextBox.GetComponent<Text>().text = "Yummy, bones!";
-        OptionSelected = 1;
-        SceneManager.LoadScene("BaseMap", LoadSceneMode.Single);
+        OptionSelected = 1;  
     }
 
     public void optionSelected2()
     {
         TextBox.GetComponent<Text>().text = "Isn't that where we first met?!";
         OptionSelected = 2;
-        SceneManager.LoadScene("BaseMapLeft", LoadSceneMode.Single);
     }
 
     public void optionSelected3()
     {
         TextBox.GetComponent<Text>().text = "I love walks!";
         OptionSelected = 3;
-        SceneManager.LoadScene("BaseMapRight", LoadSceneMode.Single);
     }
 
     public void optionSelected4()
     {
-        //TextBox.GetComponent<Text>().text = "I love walks!";
         OptionSelected = 4;
-        SceneManager.LoadScene("OpeningNarration", LoadSceneMode.Single);
     }
 
     public void optionSelected5()
     {
-        //TextBox.GetComponent<Text>().text = "I love walks!";
         OptionSelected = 5;
-        SceneManager.LoadScene("OpeningNarration", LoadSceneMode.Single);
     }
 
     public void optionSelected6()
     {
-        //TextBox.GetComponent<Text>().text = "I love walks!"; 
         OptionSelected = 6;
         Application.Quit();
     }
@@ -60,19 +64,57 @@ public class NarrationScript : MonoBehaviour
 
     private void Start()
     {
-    
+           
     }
 
     void Update()
     {
         if (OptionSelected >= 1)
         {
-            Option01.SetActive(false);
-            Option02.SetActive(false);
-            Option03.SetActive(false);
-            Option04.SetActive(false);
-            Option05.SetActive(false);
+            if (OptionSelected == 1) 
+            {
+                //Option01.SetActive(false);
+                levelToLoad = "BaseMap";
+                StartCoroutine(ExecuteAfterTime(screenDelay));
+            } 
+            else if(OptionSelected == 2)
+            {
+               //Option02.SetActive(false);
+                levelToLoad = "BaseMapLeft";
+                StartCoroutine(ExecuteAfterTime(screenDelay));
+            }
+            else if (OptionSelected == 3)
+            {
+                //Option03.SetActive(false);
+                levelToLoad = "BaseMapRight";
+                StartCoroutine(ExecuteAfterTime(screenDelay));
+            }
+            else if (OptionSelected == 4)
+            {
+               //Option04.SetActive(false);
+                levelToLoad = "OpeningNarration";
+                StartCoroutine(ExecuteAfterTime(screenDelay));
+            }
+            else if (OptionSelected == 5)
+            {
+                //Option05.SetActive(false);
+                levelToLoad = "AboutUs";
+                StartCoroutine(ExecuteAfterTime(screenDelay));
+            }
+            else if (OptionSelected == 6)
+            {
+                Option06.SetActive(false);
+                StartCoroutine(ExecuteAfterTime(screenDelay));
+            }
+            Option01.SetActive(false); 
+            Option02.SetActive(false); 
+            Option03.SetActive(false); 
+            Option04.SetActive(false); 
+            Option05.SetActive(false); 
             Option06.SetActive(false);
+
+
+
         }
 
        
