d6dcbaff79416bcbcf57d7b9b0d9b92ce2308935
Taylor <crossedge28@gmail.com>
2020-02-01 18:53:08 -0800

    Summary!
    We lost the head...

---
+++ Assets/Scripts/BodyPartObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DespairRepair;
+
+public class BodyPartObject : MonoBehaviour
+{
+    void Start() {
+        
+    }
+
+    public BodyPartTypes part;
+    public bool isCorrect;
+}
