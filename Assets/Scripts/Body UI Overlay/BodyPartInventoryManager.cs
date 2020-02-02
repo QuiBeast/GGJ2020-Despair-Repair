62e704ba5478d6614e27798aa3f6797e69bf8ff3
Matt <Osafune24@gmail.com>
2020-02-01 16:02:26 -0800

    Creating Body Overlay prefab

---
+++ Assets/Scripts/Body UI Overlay/BodyPartInventoryManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DespairRepair
+{
+    namespace BodyOverlayUI
+    {
+        public class BodyPartInventoryManager : MonoBehaviour
+        {
+            public const string HEAD_TAG = "UI-Head";
+            public const string TORSO_TAG = "UI-Torso";
+            public const string LEFT_ARM_TAG = "UI-Left-Arm";
+            public const string RIGHT_ARM_TAG = "UI-Right-Arm";
+            public const string LEFT_LEG_TAG = "UI-Left-Leg";
+            public const string RIGHT_LEG_TAG = "UI-Right-Leg";
+
+            public enum BodyParts
+            {
+                head,
+                torso,
+                leftArm,
+                rightArm,
+                leftLeg,
+                rightLeg
+            }
+
+            // Start is called before the first frame update
+            void Start()
+            {
+                
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                this.CollectBodyPart(BodyParts.torso, false);
+                print(GetNumberOfCorrectBodyPartsCollected());
+            }
+
+            public void CollectBodyPart(BodyParts bodyPart, bool isCorrectPart)
+            {
+                UIBodyPart part = this.GetBodyPart(bodyPart);
+
+                if (part != null && !part.IsCollected())
+                {
+                    part.Collect(isCorrectPart);
+                }
+            }
+
+            public bool IsBodyPartCollected(BodyParts bodyPart)
+            {
+                UIBodyPart part = this.GetBodyPart(bodyPart);
+
+                if (part != null)
+                {
+                    return part.IsCollected();
+                }
+
+                return false;
+            }
+
+            private UIBodyPart GetBodyPart(BodyParts bodyPart)
+            {
+                UIBodyPart part = null;
+
+                switch (bodyPart)
+                {
+                    case BodyParts.head:
+                        part = GameObject.FindGameObjectWithTag(HEAD_TAG).GetComponent<UIBodyPart>();
+                        break;
+                    case BodyParts.torso:
+                        part = GameObject.FindGameObjectWithTag(TORSO_TAG).GetComponent<UIBodyPart>();
+                        break;
+                    case BodyParts.leftArm:
+                        part = GameObject.FindGameObjectWithTag(LEFT_ARM_TAG).GetComponent<UIBodyPart>();
+                        break;
+                    case BodyParts.rightArm:
+                        part = GameObject.FindGameObjectWithTag(RIGHT_ARM_TAG).GetComponent<UIBodyPart>();
+                        break;
+                    case BodyParts.leftLeg:
+                        part = GameObject.FindGameObjectWithTag(LEFT_LEG_TAG).GetComponent<UIBodyPart>();
+                        break;
+                    case BodyParts.rightLeg:
+                        part = GameObject.FindGameObjectWithTag(RIGHT_LEG_TAG).GetComponent<UIBodyPart>();
+                        break;
+                }
+
+                return part;
+            }
+
+            public bool AreAllBodyPartsCollected()
+            {
+                foreach(UIBodyPart bodyPart in this.GetComponentsInChildren<UIBodyPart>())
+                {
+                    if (!bodyPart.IsCollected())
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            public int GetNumberOfCorrectBodyPartsCollected()
+            {
+                int count = 0;
+
+                foreach (UIBodyPart bodyPart in this.GetComponentsInChildren<UIBodyPart>())
+                {
+                    if (bodyPart.IsCorrect())
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+    }
+}
--- Assets/Scripts/Body UI Overlay/UIBodyPart.cs
+++ Assets/Scripts/Body UI Overlay/UIBodyPart.cs
@@ -9,18 +9,12 @@ namespace DespairRepair
         public class UIBodyPart : MonoBehaviour
         {
             private bool isCollected;
+            private bool isCorrectPart;
 
             private SpriteRenderer spriteRenderer;
             private Sprite collectedSprite;
 
             private const string SPRITE_DIRECTORY = "Sprites/body parts/";
-
-            private const string HEAD_TAG = "UI-Head";
-            private const string TORSO_TAG = "UI-Torso";
-            private const string LEFT_ARM_TAG = "UI-Left-Arm";
-            private const string RIGHT_ARM_TAG = "UI-Right-Arm";
-            private const string LEFT_LEG_TAG = "UI-Left-Leg";
-            private const string RIGHT_LEG_TAG = "UI-Right-Leg";
 
             private const string HEAD_COLLECTED_SPRITE = "head-collected";
             private const string TORSO_COLLECTED_SPRITE = "torso-collected";
@@ -33,47 +27,57 @@ namespace DespairRepair
             void Start()
             {
                 this.isCollected = false;
+                this.isCorrectPart = false;
                 this.spriteRenderer = GetComponent<SpriteRenderer>();
-                //this.collectedSprite = Resources.Load<Sprite>("Sprites/body parts/head-collected");
             }
 
             // Update is called once per frame
             void Update()
             {
-                //print(this.collectedSprite);
-                Collect();
+                //Collect();
             }
 
-            public void Collect()
+            public void Collect(bool isCorrectPart)
             {
-                isCollected = true;
+                this.isCollected = true;
+                this.isCorrectPart = isCorrectPart;
 
-                print(this.tag);
-                if (this.tag == HEAD_TAG)
+                if (this.tag == BodyPartInventoryManager.HEAD_TAG)
                 {
-                    print(SPRITE_DIRECTORY + HEAD_COLLECTED_SPRITE);
                     this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + HEAD_COLLECTED_SPRITE);
                 }
-                else if (this.tag == TORSO_TAG)
+                else if (this.tag == BodyPartInventoryManager.TORSO_TAG)
                 {
                     this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + TORSO_COLLECTED_SPRITE);
                 }
-                else if (this.tag == LEFT_ARM_TAG)
+                else if (this.tag == BodyPartInventoryManager.LEFT_ARM_TAG)
                 {
                     this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + LEFT_ARM_COLLECTED_SPRITE);
                 }
-                else if (this.tag == RIGHT_ARM_TAG)
+                else if (this.tag == BodyPartInventoryManager.RIGHT_ARM_TAG)
                 {
                     this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + RIGHT_ARM_COLLECTED_SPRITE);
                 }
-                else if (this.tag == LEFT_LEG_TAG)
+                else if (this.tag == BodyPartInventoryManager.LEFT_LEG_TAG)
                 {
                     this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + LEFT_LEG_COLLECTED_SPRITE);
                 }
-                else if (this.tag == RIGHT_LEG_TAG)
+                else if (this.tag == BodyPartInventoryManager.RIGHT_LEG_TAG)
                 {
                     this.spriteRenderer.sprite = Resources.Load<Sprite>(SPRITE_DIRECTORY + RIGHT_LEG_COLLECTED_SPRITE);
                 }
+
+                //this.spriteRenderer.color = Color.red;//211,29,0
+            }
+
+            public bool IsCollected()
+            {
+                return this.isCollected;
+            }
+
+            public bool IsCorrect()
+            {
+                return this.isCorrectPart;
             }
         }
     }
