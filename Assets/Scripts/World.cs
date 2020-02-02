7f3b78432c597e4c82a5bab9827a97687908f388
Marqui Simmons <marqui.simmons@gmail.com>
2020-02-02 13:18:58 -0800

    BaseMap Big Update

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -9,16 +9,31 @@ public class World : SceneController
     // Use this for initialization
     public override void Start()
     {
+        gameObject.tag = "GameController";
         base.Start();
+        Debug.Log("Previous Scene: " + prevScene);
+        Debug.Log("Current Scene: " + currentScene);
+        GameObject player = GameObject.FindWithTag("Player");
 
         if (prevScene == "BaseMapLeft" && currentScene == "BaseMap")
         {
-            GameObject player = GameObject.FindWithTag("Player");
             player.transform.position = new Vector2(-8f, 0f);
         } else if(prevScene == "BaseMapRight" && currentScene == "BaseMap")
         {
-            GameObject player = GameObject.FindWithTag("Player");
             player.transform.position = new Vector2(8f, 0f);
+        } else if(prevScene == "BaseMapLeft" && currentScene == "BaseMapLeft2")
+        {
+            player.transform.position = new Vector2(7f, 6f);
+        } else if(prevScene == "BaseMapLeft2" && currentScene == "BaseMapLeft")
+        {
+            player.transform.position = new Vector2(7f, -6f);
+        } else if (prevScene == "BaseMap" && currentScene == "BaseMapLeft")
+        {   
+            player.transform.position = new Vector2(8f, 0f);
+        } else if (prevScene == "BaseMap" && currentScene == "BaseMapRight")
+        {
+            player.transform.position = new Vector2(-8f, 0f);
         }
+
     }
 }
