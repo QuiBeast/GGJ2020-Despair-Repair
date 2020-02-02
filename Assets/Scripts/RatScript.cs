5b0dfaf8d750cc674ab7fbcea3f2d68318aaed4d
Matt <Osafune24@gmail.com>
2020-02-02 12:12:18 -0800

    Restore body parts on enemy collision

--- Assets/Scripts/RatScript.cs
+++ Assets/Scripts/RatScript.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DespairRepair;
 
 public class RatScript : Enemy
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -15,8 +15,5 @@ public class RatScript : Enemy
     {
         
     }
-
-    void processMovement() {
     
-    }
 }
